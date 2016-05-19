766d40c079f0f1c4b4473c4122974a7ed5ba5fba
Artak Hamazaspyan <artak.hamazaspyan@gmail.com>
2016-05-19 19:16:27 -0400

    Remove Duplicates From Array of numbers
    implementation of 3 methods, all different styles

---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates
+{
+    public class Program
+    {
+        private static readonly int[] RandomIntegers = {1,2,34,34,25,1,45,3,26,85,4,34,86,25,43,2,1,10000,11,16,19,1,18,4,9,3,
+                                   20,17,8,15,6,2,5,10,14,12,13,7,8,9,1,2,15,12,18,10,14,20,17,16,3,6,19,
+                                   13,5,11,4,7,19,16,5,9,12,3,20,7,15,17,10,6,1,8,18,4,14,13,2,11};   
+
+        public static void Main(string[] args)
+        {
+            RemoveDuplicatesMethodFirst(RandomIntegers);
+            RemoveDuplicatesMethodSecond(RandomIntegers);
+            RemoveDuplicatesMethodThird(RandomIntegers);
+        }
+
+        public static List<int> RemoveDuplicatesMethodFirst(int[] randomIntegers)
+        {
+            var start = DateTime.Now;
+            var result = new List<int>();
+            foreach (var number in randomIntegers.Where(number => !result.Contains(number)))
+            {
+                result.Add(number);
+            }
+            Console.WriteLine("RemoveDuplicatesMethodFirst runtime is {0} ticks", (DateTime.Now - start).TotalMilliseconds);
+            Console.WriteLine("RemoveDuplicatesMethodFirst result:");
+            Console.Write("[");
+            var count = 0;
+            foreach (var item in result)
+            {
+                Console.Write(item);
+                count++;
+                if (result.Count > count)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine("]");
+             
+            return result;
+        }
+
+        public static List<int> RemoveDuplicatesMethodSecond(int[] randomIntegers)
+        {
+            var start = DateTime.Now;
+            var result = randomIntegers.Distinct().ToList();
+            Console.WriteLine("RemoveDuplicatesMethodSecond runtime is {0} ticks", (DateTime.Now - start).TotalMilliseconds);
+            Console.WriteLine("RemoveDuplicatesMethodSecond result:");
+            Console.Write("[");
+            var count = 0;
+            foreach (var item in result)
+            {
+                Console.Write(item);
+                count++;
+                if (result.Count > count)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine("]");
+            return result;
+        }
+
+        public static List<int> RemoveDuplicatesMethodThird(int[] randomIntegers)
+        {
+            var start = DateTime.Now;
+            var result = new HashSet<int>(randomIntegers).ToList();
+
+            Console.WriteLine("RemoveDuplicatesMethodThird runtime is {0} ticks", (DateTime.Now - start).TotalMilliseconds);
+            Console.WriteLine("RemoveDuplicatesMethodThird result:");
+            Console.Write("[");
+            var count = 0;
+            foreach (var item in result)
+            {
+                Console.Write(item);
+                count++;
+                if (result.Count > count)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine("]");
+            
+            return result;
+        }
+    }
+}
---
+++ RemoveDuplicatesUnitTestProject/ProgramTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoveDuplicates;
+namespace RemoveDuplicatesUnitTestProject
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void RemoveDuplicatesMethodFirstTest()
+        {
+            var actualValue = Program.RemoveDuplicatesMethodFirst(new int[] {1, 2, 2, 3, 4, 5, 5, 6});
+            var expectedValue = new List<int> { 1, 2, 3, 4, 5, 6 };
+            Assert.IsTrue(actualValue.SequenceEqual(expectedValue));
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesMethodSecondTest()
+        {
+            var actualValue = Program.RemoveDuplicatesMethodSecond(new int[] { 1, 2, 2, 3, 4, 5, 5, 6 });
+            var expectedValue = new List<int> { 1, 2, 3, 4, 5, 6 };
+            Assert.IsTrue(actualValue.SequenceEqual(expectedValue));
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesMethodThirdTest()
+        {
+            var actualValue = Program.RemoveDuplicatesMethodThird(new int[] { 1, 2, 2, 3, 4, 5, 5, 6 });
+            var expectedValue = new List<int> { 1, 2, 3, 4,  5, 6 };
+            Assert.IsTrue(actualValue.SequenceEqual(expectedValue));
+        }
+    }
+}
